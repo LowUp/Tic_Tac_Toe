da2d819a58fa71a3f10b9147acced5f432c04e74
LowUp <credo05@hotmail.fr>
2022-02-21 02:36:58 +0100

    Setting up the environment

---
+++ app.py
@@ -0,0 +1,11 @@
+import random
+
+colomn_range = 3
+blank = "-"
+
+game_board = [
+    [blank for i in range(colomn_range)],
+    [blank for i in range(colomn_range)],
+    [blank for i in range(colomn_range)]
+]
+
