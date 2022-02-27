965cf279364122f37a1f5ddfa4a6a91e7d559eb4
LowUp <credo05@hotmail.fr>
2022-02-27 21:39:18 +0100

    Retry feature
    When the player chooses a position that has already been played, the program will infor the player and will let the player to retry

--- Rules.py
+++ Rules.py
@@ -10,31 +10,31 @@ class Rules:
     def set_board(self, num, symbol):
         if num == 7:
             self.game_board[0][0] = symbol
-            print("You played at position (0,0)")
+            print("You played at position (1,1)")
         elif num == 8:
             self.game_board[0][1] = symbol
-            print("You played at position (0,1)")
+            print("You played at position (1,2)")
         elif num == 9:
             self.game_board[0][2] = symbol
-            print("You played at position (0,2)")
+            print("You played at position (1,3)")
         elif num == 4:
             self.game_board[1][0] = symbol
-            print("You played at position (1,0)")
+            print("You played at position (2,1)")
         elif num == 5:
             self.game_board[1][1] = symbol
-            print("You played at position (1,1)")
+            print("You played at position (2,2)")
         elif num == 6:
             self.game_board[1][2] = symbol
-            print("You played at position (1,2)")
+            print("You played at position (2,3)")
         elif num == 1:
             self.game_board[2][0] = symbol
-            print("You played at position (2,0)")
+            print("You played at position (3,1)")
         elif num == 2:
             self.game_board[2][1] = symbol
-            print("You played at position (2,1)")
+            print("You played at position (3,2)")
         elif num == 3:
             self.game_board[2][2] = symbol
-            print("You played at position (2,2)")
+            print("You played at position (3,3)")
         else:
             print("Invalid input")
 
--- app.py
+++ app.py
@@ -23,6 +23,12 @@ player1 = Rules(game_board)
 player2 = Rules(game_board)
 ref_board = Rules(reference_board)
 
+def player_details():
+    print("Reference board")
+    ref_board.get_board()
+    print("Tic Tac Toe")
+    player1.get_board()
+
 def play():
 
     played_position = ["" for i in range(9)]
@@ -30,7 +36,6 @@ def play():
 
     while True:
         player1_symbol = input("Player 1 choose your symbol (O or X): ")
-        player2_symbol = ""
         if (player1_symbol == "O" or player1_symbol == "o") or (player1_symbol == "X" or player1_symbol == "x"):
             print("Success")
             if player1_symbol == "O" or player1_symbol == "o":
@@ -43,34 +48,34 @@ def play():
             print("Wrong input! \nTry again !")
     while True:
         try:
-            print("Reference board")
-            ref_board.get_board()
-            print("Tic Tac Toe")
-            player1.get_board()
-            player1_input = int(input("Player 1 choose a position from 1 to 9: "))
+            flag1 = True
+            while flag1:
+                player_details()
+                player1_input = int(input(f"Player 1 choose a position from 1 to 9: "))
 
-            if player1_input in played_position:
-                print("This position has already been played")
-        
-            else:
-                player1.set_board(player1_input, player1_symbol.upper())
-                played_position[counter] = player1_input
-                counter += 1
-                print(f"played position {played_position}")
+                if player1_input in played_position:
+                    print("This position has already been played\nRetry !")
 
-            print("Reference board")
-            ref_board.get_board()
-            print("Tic Tac Toe")
-            player1.get_board()
-            player2_input = int(input("Player 2 choose a position from 1 to 9: "))
+                else:
+                    flag1 = False
+                    player1.set_board(player1_input, player1_symbol.upper())
+                    played_position[counter] = player1_input
+                    counter += 1
+                    print(f"played position {played_position}")
 
-            if player2_input in played_position:
-                print("This position has already been played")
-            else:
-                player2.set_board(player2_input, player2_symbol.upper())
-                played_position[counter] = player2_input
-                counter += 1
-                print(f"played position {played_position}")
+            flag2 = True
+            while flag2:
+                player_details()
+                player2_input = int(input("Player 2 choose a position from 1 to 9: "))
+
+                if player2_input in played_position:
+                    print("This position has already been played\n Retry")
+                else:
+                    flag2 = False
+                    player2.set_board(player2_input, player2_symbol.upper())
+                    played_position[counter] = player2_input
+                    counter += 1
+                    print(f"played position {played_position}")
 
         except(ValueError):
             print("Wrong input")
