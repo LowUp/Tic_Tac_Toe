f841be5e7a60176d16b4a7343157d678bcfc71ba
LowUp <credo05@hotmail.fr>
2022-03-18 22:51:25 +0100

    implemented an unbeatable ai
    Made two types of AI:
     - An easy mode
     - and a hard mode

--- Rules.py
+++ Rules.py
@@ -13,34 +13,43 @@ class Player:
     def game_board(self):
         return self.board
 
-    def set_board(self, num, symbol):
+    def set_board(self, num, symbol, display):
         if num == 7:
             self.game_board()[0][0] = symbol
-            print("You played at position (1,1)")
+            if display:
+                print("You played at position (1,1)")
         elif num == 8:
             self.game_board()[0][1] = symbol
-            print("You played at position (1,2)")
+            if display:
+                print("You played at position (1,2)")
         elif num == 9:
             self.game_board()[0][2] = symbol
-            print("You played at position (1,3)")
+            if display:
+                print("You played at position (1,3)")
         elif num == 4:
             self.game_board()[1][0] = symbol
-            print("You played at position (2,1)")
+            if display:
+                print("You played at position (2,1)")
         elif num == 5:
             self.game_board()[1][1] = symbol
-            print("You played at position (2,2)")
+            if display:
+                print("You played at position (2,2)")
         elif num == 6:
             self.game_board()[1][2] = symbol
-            print("You played at position (2,3)")
+            if display:
+                print("You played at position (2,3)")
         elif num == 1:
             self.game_board()[2][0] = symbol
-            print("You played at position (3,1)")
+            if display:
+                print("You played at position (3,1)")
         elif num == 2:
             self.game_board()[2][1] = symbol
-            print("You played at position (3,2)")
+            if display:
+                print("You played at position (3,2)")
         elif num == 3:
             self.game_board()[2][2] = symbol
-            print("You played at position (3,3)")
+            if display:
+                print("You played at position (3,3)")
         else:
             print("Invalid input")
 
@@ -63,34 +72,43 @@ class ComputerPlayer(Player):
     def __init__(self, board, player_name):
         super().__init__(board, player_name)
 
-    def set_board(self, num, symbol):
+    def set_board(self, num, symbol, display):
         if num == 7:
             self.game_board()[0][0] = symbol
-            print("The AI played at position (1,1)")
+            if display:
+                print("The AI played at position (1,1)")
         elif num == 8:
             self.game_board()[0][1] = symbol
-            print("The AI played at position (1,2)")
+            if display:
+                print("The AI played at position (1,2)")
         elif num == 9:
             self.game_board()[0][2] = symbol
-            print("The AI played at position (1,3)")
+            if display:
+                print("The AI played at position (1,3)")
         elif num == 4:
             self.game_board()[1][0] = symbol
-            print("The AI played at position (2,1)")
+            if display:
+                print("The AI played at position (2,1)")
         elif num == 5:
             self.game_board()[1][1] = symbol
-            print("The AI played at position (2,2)")
+            if display:
+                print("The AI played at position (2,2)")
         elif num == 6:
             self.game_board()[1][2] = symbol
-            print("The AI played at position (2,3)")
+            if display:
+                print("The AI played at position (2,3)")
         elif num == 1:
             self.game_board()[2][0] = symbol
-            print("The AI played at position (3,1)")
+            if display:
+                print("The AI played at position (3,1)")
         elif num == 2:
             self.game_board()[2][1] = symbol
-            print("The AI played at position (3,2)")
+            if display:
+                print("The AI played at position (3,2)")
         elif num == 3:
             self.game_board()[2][2] = symbol
-            print("The AI played at position (3,3)")
+            if display:
+                print("The AI played at position (3,3)")
         else:
             print("Invalid input")
 
@@ -112,12 +130,8 @@ class UnbeatableAI(ComputerPlayer):
     def __init__(self, board, player_name):
         super().__init__(board, player_name)
 
-    def get_move(self, position):
-        return position
-
-    # def minimax(self, player):
-    #    max_player = self.player_name
-    #    other_player = "other_player"
+    def get_move(self, move):
+        return move
 
 
 # Game class
@@ -165,4 +179,59 @@ class Game:
         elif num == 1:
             diagonal = [self.game_board()[0][2], self.game_board()[1][1], self.game_board()[2][0]]
             return diagonal
+
+    def board_keys(self, num):
+        if num == 7:
+            return self.game_board()[0][0]
+        elif num == 8:
+            return self.game_board()[0][1]
+        elif num == 9:
+            return self.game_board()[0][2]
+        elif num == 4:
+            return self.game_board()[1][0]
+        elif num == 5:
+            return self.game_board()[1][1]
+        elif num == 6:
+            return self.game_board()[1][2]
+        elif num == 1:
+            return self.game_board()[2][0]
+        elif num == 2:
+            return self.game_board()[2][1]
+        elif num == 3:
+            return self.game_board()[2][2]
+        else:
+            print("Invalid")
+
+    def set_board(self, num, symbol):
+        if num == 7:
+            self.game_board()[0][0] = symbol
+        elif num == 8:
+            self.game_board()[0][1] = symbol
+        elif num == 9:
+            self.game_board()[0][2] = symbol
+        elif num == 4:
+            self.game_board()[1][0] = symbol
+        elif num == 5:
+            self.game_board()[1][1] = symbol
+        elif num == 6:
+            self.game_board()[1][2] = symbol
+        elif num == 1:
+            self.game_board()[2][0] = symbol
+        elif num == 2:
+            self.game_board()[2][1] = symbol
+        elif num == 3:
+            self.game_board()[2][2] = symbol
+        else:
+            print("Invalid")
+
+    def draw(self):
+
+        flag = True
+
+        for key in range(1, 10):
+            if self.board_keys(key) == "-":
+                flag = False
+
+        return flag
+
 
--- app.py
+++ app.py
@@ -5,91 +5,147 @@ from Rules import ComputerPlayer
 from Rules import UnbeatableAI
 from Rules import Game
 
-board = Game()
+board1 = Game()
 
-game_board = board.game_board()
+game_board = board1.game_board()
 
 player1 = Player(game_board, input("Player 1 enter your username: ").upper())
-
-playerTest = UnbeatableAI(game_board, "AItest")
 
 # Decides if player 2 will be an AI or a human player
 player2_username = input("Player 2 enter your username: ").upper()
 
 if player2_username == "AI":
-    player2 = ComputerPlayer(game_board, player2_username)
+    choice = input("choose the level difficulty of the AI\n(e = easy) or (h = hard): ").upper()
+
+    if choice == "H" or choice == "HARD":
+        player2 = UnbeatableAI(game_board, player2_username)
+    elif choice == "E" or choice == "EASY":
+        player2 = ComputerPlayer(game_board, player2_username)
+    else:
+        print("Invalid input\nEasy will be selected by default")
+        player2 = ComputerPlayer(game_board, player2_username)
 else:
     player2 = Player(game_board, player2_username)
 
 
 def player_details():
-    board.get_refBoard()
-    board.get_board()
+    board1.get_refBoard()
+    board1.get_board()
 
 
-def check_rows(playerName, playerSymbol):
+def check_rows(player_name, player_symbol, display):
     rows_number = 3
-    win = f'{playerSymbol}{playerSymbol}{playerSymbol}'
+    win = f'{player_symbol}{player_symbol}{player_symbol}'
 
     for i in range(rows_number):
-        row = ''.join(board.get_row(i))
+        row = ''.join(board1.get_row(i))
         if row == win:
-            board.get_board()
-            print(f"\n{playerName} wins")
+            if display:
+                board1.get_board()
+                print(f"\n{player_name} wins")
             return True
 
 
-def check_columns(playerName, playerSymbol):
+def check_columns(player_name, player_symbol, display):
     columns_number = 3
-    win = f'{playerSymbol}{playerSymbol}{playerSymbol}'
+    win = f'{player_symbol}{player_symbol}{player_symbol}'
 
     for i in range(columns_number):
-        column = ''.join(board.get_column(i))
+        column = ''.join(board1.get_column(i))
         if column == win:
-            board.get_board()
-            print(f"\n{playerName} wins")
+            if display:
+                board1.get_board()
+                print(f"\n{player_name} wins")
             return True
 
 
-def check_diagonals(playerName, playerSymbol):
+def check_diagonals(player_name, player_symbol, display):
 
     diagonals_number = 2
-    win = f'{playerSymbol}{playerSymbol}{playerSymbol}'
+    win = f'{player_symbol}{player_symbol}{player_symbol}'
 
     for i in range(diagonals_number):
-        diagonal = ''.join(board.get_diagonal(i))
+        diagonal = ''.join(board1.get_diagonal(i))
         if diagonal == win:
-            board.get_board()
-            print(f"\n{playerName} wins")
+            if display:
+                board1.get_board()
+                print(f"\n{player_name} wins")
             return True
 
-# def hard_ai (symbol):
-#    bestScore = -1000
-#    bestMove = 0
 
-#    for key in game_board:
-#        if(game_board[key] == "-"):
-#            game_board[key] = symbol
-#            score = minimax(game_board,0,False)
-#            game_board[key] = "-"
-#            if(score > bestScore):
-#                bestScore = score
-#                bestMove = key
+def hard_mode_ai(board):
+    best_score = -1000
+    best_move = 0
+    conteur = 0
 
-#    return playerTest.get_move(bestMove)
+    for key in range(1, 10):
+        # print(board.board_keys(key))
+        if board.board_keys(key).upper() == "-".upper():
+            # print(board.board_keys(key))
+            player2.set_board(key, player2.get_symbol(), False)
+            score = minimax(board, 0, False, conteur)
+            board.set_board(key, "-")
+            if score > best_score:
+                best_score = score
+                best_move = key
 
-# def minimax(board, depth, isMaximizing):
+    return best_move
 
-   # if check_rows(player1.get_player_name(), player1_symbol.upper()) \
-   #         or check_columns(player1.get_player_name(), player1_symbol.upper()) \
-   #         or check_diagonals(player1.get_player_name(), player1_symbol.upper()):
+
+def minimax(board, depth, is_maximizing, count):
+
+    # checks if player 1 wins
+    if check_rows(player1.get_player_name(), player1.get_symbol(), False) \
+            or check_columns(player1.get_player_name(), player1.get_symbol(), False) \
+            or check_diagonals(player1.get_player_name(), player1.get_symbol(), False):
+        return -100
+
+    # checks if player 2 wins
+    elif check_rows(player2.get_player_name(), player2.get_symbol(), False) \
+            or check_columns(player2.get_player_name(), player2.get_symbol(), False) \
+            or check_diagonals(player2.get_player_name(), player2.get_symbol(), False):
+        return 100
+
+    # checks for draws
+    elif board.draw():
+        # I have to implement a function that make the computer understand when the game ties
+        return 0
+
+    # The computer plays against itself to check all the possibilities and choose the right one
+    if is_maximizing:
+        best_score = -1000
+
+        for key in range(1, 10):
+            if board.board_keys(key).upper() == "-".upper():
+                player2.set_board(key, player2.get_symbol(), False)
+                score = minimax(board, 0, False, count)
+                board.set_board(key, "-")
+                count += 1
+                if score > best_score:
+                    best_score = score
+
+        return best_score
+
+    else:
+        best_score = 800
+
+        for key in range(1, 10):
+            if board.board_keys(key).upper() == "-".upper():
+                player1.set_board(key, player1.get_symbol(), False)
+                score = minimax(board, depth + 1, True, count)
+                board.set_board(key, "-")
+                count += 1
+                if score < best_score:
+                    best_score = score
+
+        return best_score
 
 
 def play():
 
     # Players choose symbols (X or O)
     played_position = ["" for i in range(9)]
-    counter = 0
+    counter: int = 0
 
     while True:
         player1_symbol = input(f"{player1.get_player_name()} choose your symbol (O or X): ").upper()
@@ -121,21 +177,21 @@ def play():
             if player1_input in played_position:
                 print("This position has already been played\nRetry !")
 
-            elif player1_input in range(1,10):
+            elif player1_input in range(1, 10):
                 if player2_username == "AI":
                     player2.deleteMove(player1_input)
                 flag1 = False
-                player1.set_board(player1_input, player1.get_symbol())
+                player1.set_board(player1_input, player1.get_symbol(), True)
                 played_position[counter] = player1_input
                 counter += 1
                 print(f"played position {played_position}")
 
                 # Checks if the player wins the game or if tie
-                if check_rows(player1.get_player_name(), player1.get_symbol()) \
-                        or check_columns(player1.get_player_name(), player1.get_symbol())\
-                        or check_diagonals(player1.get_player_name(), player1.get_symbol()):
+                if check_rows(player1.get_player_name(), player1.get_symbol(), True) \
+                        or check_columns(player1.get_player_name(), player1.get_symbol(), True)\
+                        or check_diagonals(player1.get_player_name(), player1.get_symbol(), True):
                     return player1.winner()
-                elif counter >= 9:
+                elif board1.draw():
                     player_details()
                     print("\n Tie")
                     return True
@@ -148,7 +204,10 @@ def play():
             player_details()
 
             if player2_username == "AI":
-                player2_input = player2.get_move()
+                if choice == "H" or choice == "HARD":
+                    player2_input = player2.get_move(hard_mode_ai(board1))
+                else:
+                    player2_input = player2.get_move()
             else:
                 try:
                     player2_input = int(input(f"{player2.get_player_name()} choose a position from 1 to 9: "))
@@ -158,22 +217,24 @@ def play():
             if player2_input in played_position:
                 print("This position has already been played\nRetry")
 
-            elif player2_input in range(1,10):
+            elif player2_input in range(1, 10):
                 if player2_username == "AI":
                     player2.deleteMove(player2_input)
                     time.sleep(0.5)
+                if choice == "H" or choice == "HARD":
+                    player2.deleteMove(player2_input)
                 flag2 = False
-                player2.set_board(player2_input, player2.get_symbol())
+                player2.set_board(player2_input, player2.get_symbol(), True)
                 played_position[counter] = player2_input
                 counter += 1
                 print(f"played position {played_position}")
 
                 # Checks if the player wins the game or if tie
-                if check_rows(player2.get_player_name(), player2.get_symbol())\
-                        or check_columns(player2.get_player_name(), player2.get_symbol())\
-                        or check_diagonals(player2.get_player_name(), player2.get_symbol()):
+                if check_rows(player2.get_player_name(), player2.get_symbol(), True)\
+                        or check_columns(player2.get_player_name(), player2.get_symbol(), True)\
+                        or check_diagonals(player2.get_player_name(), player2.get_symbol(), True):
                     return player2.winner()
-                elif counter >= 9:
+                elif board1.draw():
                     player_details()
                     print("\n Tie")
                     return True
